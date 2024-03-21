3aeb7a481c6a9368cbc26cede6a8adbe0c0f6b27
Judson James <judsonjames96@gmail.com>
2024-03-21 16:24:02 -0400

    Fix OPR Formatting

--- pages/4_OPRs.py
+++ pages/4_OPRs.py
@@ -3,6 +3,9 @@ import streamlit as st
 import pandas as pd
 import os
 
+from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid.shared import ExcelExportMode
+
 from query_data import load_tba_opr_data, load_tba_opr_options
 
 event_options: List[str] = load_tba_opr_options()
@@ -13,4 +16,22 @@ event = st.selectbox(
 )
 
 if event:
-    st.dataframe(load_tba_opr_data(event=event))
+    data = load_tba_opr_data(event=event)
+    st.header("TBA COPRs")
+
+    data_grid = GridOptionsBuilder.from_dataframe(data)
+    data_grid.configure_pagination(enabled=True, paginationPageSize=10)
+    data_grid.configure_default_column(groupable="true", filterable="true")
+    data_grid.configure_grid_options(alwaysShowHorizontalScroll=True)
+    data_grid.configure_side_bar(filters_panel=True)
+
+    AgGrid(data,
+        gridOptions=data_grid.build(),
+        excel_export_mode=ExcelExportMode.MANUAL,
+        allow_unsafe_jscode=True,
+        custom_css={
+            "#gridToolBar": {
+                "padding-bottom": "0px !important",
+            }
+        },
+    )
